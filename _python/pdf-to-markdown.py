1e7b5612250d155114215571c5c7a7145cd6199e
mat-0 <Github@thechels.uk>
2025-04-12 00:30:01 +0100

    updates
    Signed-off-by: mat-0 <Github@thechels.uk>

---
+++ _python/pdf-to-markdown.py
@@ -0,0 +1,23 @@
+import config
+import os
+from markitdown import MarkItDown
+
+
+path = config.PATH
+FOLDER = f"{path}/converts/"
+
+
+
+if __name__ == '__main__':
+    for filename in os.listdir(FOLDER):
+        if filename.endswith('.pdf'):
+            input_file = os.path.join(FOLDER, filename)
+            output_file = os.path.join(FOLDER, f"{os.path.splitext(filename)[0]}.md")
+
+            md = MarkItDown()
+            result = md.convert(input_file)
+            outcome = result.markdown
+
+            with open(output_file, 'w', encoding='utf-8') as f:
+                f.write(outcome)
+                print(f"Markdown content written to {output_file}")
